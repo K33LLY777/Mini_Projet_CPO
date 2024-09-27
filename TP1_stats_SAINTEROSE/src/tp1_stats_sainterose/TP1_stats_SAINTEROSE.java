a5fea841603e7fb74d0607aa96c98a40b8ef2383
saint <saint@10.3.61.213>
2024-09-27 11:01:05 +0200

    Création d'un nouveau projet versionné

--- TP1_stats_SAINTEROSE/src/tp1_stats_sainterose/TP1_stats_SAINTEROSE.java
+++ TP1_stats_SAINTEROSE/src/tp1_stats_sainterose/TP1_stats_SAINTEROSE.java
@@ -6,27 +6,40 @@ import java.util.Scanner;
 public class TP1_stats_SAINTEROSE {
 
     public static void main(String[] args) {
-        // Déclarer un tableau de taille 6
-        int tab[] = new int[6];
-        int i = 0; // Initialiser le compteur i
-
-        // Demander à l'utilisateur une valeur entière
-        System.out.println("\nSaisissez une valeur entière (le nombre d'itérations souhaitées) :");
-        Scanner scann = new Scanner(System.in);  // Correction de la syntaxe
-        int m = scann.nextInt();  // Lire l'entier entré par l'utilisateur
-
-        // Boucle while pour générer des valeurs aléatoires et les stocker dans le tableau
-        while (i < m && i < tab.length) {  // S'assurer que m ne dépasse pas la taille du tableau (6)
-            Random generateurAleat = new Random();
-            int n = generateurAleat.nextInt(6);  // Génére un nombre aléatoire entre 0 et 5
-            tab[i] = n;  // Stocker la valeur dans le tableau
-            i++;  // Incrémenter le compteur
+        
+        // Crée un générateur de nombres aléatoires
+        Random random = new Random();
+        
+        // Compteurs pour chaque face de dé (de 1 à 6)
+        int compteur1 = 0, compteur2 = 0, compteur3 = 0, compteur4 = 0, compteur5 = 0, compteur6 = 0;
+        
+        // Demande à l'utilisateur combien de lancers il souhaite effectuer
+        System.out.println("\nVeuillez saisir un nombre de lancers de de :");
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();  // Nombre de lancers
+        
+        // Simulation de lancers de dé
+        for (int i = 0; i < m; i++) {
+            int resultat = random.nextInt(6) + 1;  // Génère un nombre entre 1 et 6
+            
+            // Utilisation du switch pour mettre à jour les compteurs
+            switch (resultat) {
+                case 1 -> compteur1++;
+                case 2 -> compteur2++;
+                case 3 -> compteur3++;
+                case 4 -> compteur4++;
+                case 5 -> compteur5++;
+                case 6 -> compteur6++;
+            }
         }
-
-        // Afficher les valeurs du tableau générées
-        System.out.println("Voici les valeurs générées : ");
-        for (int j = 0; j < i; j++) {
-            System.out.println("Valeur " + (j+1) + " : " + tab[j]);
-        }
+        
+        // Affiche les résultats des lancers
+        System.out.println("Resultats des lancers de de :");
+        System.out.println("Face 1 : " + compteur1);
+        System.out.println("Face 2 : " + compteur2);
+        System.out.println("Face 3 : " + compteur3);
+        System.out.println("Face 4 : " + compteur4);
+        System.out.println("Face 5 : " + compteur5);
+        System.out.println("Face 6 : " + compteur6);
     }
 }
